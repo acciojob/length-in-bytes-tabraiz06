3e60ad27a94ac47497a2f17060a0e38ea6ba6f0b
Md Tabraiz <138916881+tabraiz06@users.noreply.github.com>
2024-10-16 12:17:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const byteSize = (str) => {
   // write your code here
+	
+	return str.split("").length
+	
 };
 
 // Do not change the code below
+
 const str = prompt("Enter some string.");
 alert(byteSize(str));
